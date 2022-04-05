9db29cf3856c1c4bb7dce4d78dca1778142db9bc
25327 <253277@gmail.com>
2022-04-05 21:32:43 +0300

    created homepage login form

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'test';
+?>
+
+<form action="#">
+    <fieldset>
+        <legend>Prisijungimas:</legend>
+        Paštas: <input type="email" id="email" name="email">
+        <br><br>
+        Slaptažodis: <input type="text" id="password" name="password">
+        <br><br>
+        <button type="submit">Prisijungti</button>
+        <hr>
+        Neturite paskyros? Užsiregistruokite!
+        <br>
+        <a href="#">Registruotis</a>
+    </fieldset>
+</form>
