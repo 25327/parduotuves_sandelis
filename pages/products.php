aa07db6f20d5e3d04c60a716418fb708bdbea09a
25327 <253277@gmail.com>
2022-04-07 19:23:15 +0300

    created form, sql code to save products to database

--- pages/products.php
+++ pages/products.php
@@ -1,2 +1,53 @@
 <?php
-echo 'test products';
+if (isset($_POST['pavadinimas'])) {
+    $saveProduct = 'insert into produktai (kategorija, pavadinimas, kaina, galiojimo_dienos) value ("' . $_POST['kategorija'] . '", "' . $_POST['pavadinimas'] . '", "' . $_POST['kaina'] . '", "' . $_POST['galiojimo_dienos'] . '")';
+    mysqli_query($database, $saveProduct);
+}
+?>
+
+<h1>Produktai</h1>
+
+<form action="index.php?page=products&action=save" method="post">
+    <table>
+        <tr>
+            <td>
+                Kategorija
+            </td>
+            <td>
+                <input type="text" name="kategorija">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Prekės pavadinimas
+            </td>
+            <td>
+                <input type="text" name="pavadinimas">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Kaina
+            </td>
+            <td>
+                <input type="number" name="kaina">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Galiojimo laikas (dienomis):
+            </td>
+            <td>
+                <select name="galiojimo_dienos">
+                    <option value="">-</option>
+                    <?php for ($i = 1; $i <= 20; $i++) { ?>
+                        <option value="<?php echo $i; ?>">
+                            <?php echo $i ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </td>
+        </tr>
+    </table>
+    <button type="submit">Išsaugoti</button>
+</form>
